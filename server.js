8440068c659abbb8539ce305233e354ee88a60f4
Selcuk <selcuk.guler00@gmail.com>
2023-01-19 18:15:14 +0300

    added fullscreen

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.static(path.join(__dirname, './dist')))
 app.get('/', (req, res) => res.redirect('/lets-smile-emoji'))
 app.get('/lets-smile', (req, res) => res.sendFile(path.join(viewsDir, 'webcamFaceExpressionRecognition.html')))
 app.get('/lets-smile-emoji', (req, res) => res.sendFile(path.join(viewsDir, 'smileFaceEmoji.html')))
+app.get('/fullscreen', (req, res) => res.sendFile(path.join(viewsDir, 'fullScreen.html')))
 
 app.post('/fetch_external_image', async (req, res) => {
   const { imageUrl } = req.body
